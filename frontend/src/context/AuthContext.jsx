51a9aac590bbbff65cbde1bd29862abf26391ca4
SneedUltima <111998973+SneedUltima@users.noreply.github.com>
2022-12-23 21:23:53 +1030

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -10,7 +10,7 @@ export const authReducer = (state, action) => {
       };
     case "LOGOUT":
       return {
-        messages: null,
+        user: null,
       };
     default:
       return state;
